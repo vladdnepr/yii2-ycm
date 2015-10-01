8b3453946f2388d0a990dc99bbd27714d19a754e
Vladislav Lyshenko <lyshenko@devoler.com>
2015-10-01 10:21:14 +0300

    migrate controller choices and editable

--- src/controllers/ModelController.php
+++ src/controllers/ModelController.php
@@ -3,15 +3,19 @@
 namespace vladdnepr\ycm\controllers;
 
 use kartik\grid\GridView;
+use vladdnepr\ycm\Module;
+use vladdnepr\ycm\utils\helpers\ModelHelper;
 use Yii;
 use vova07\imperavi\helpers\FileHelper as RedactorFileHelper;
 use yii\base\DynamicModel;
 use yii\base\InvalidConfigException;
 use yii\data\ActiveDataProvider;
+use yii\db\ActiveRecord;
 use yii\filters\AccessControl;
 use yii\filters\VerbFilter;
 use yii\helpers\FileHelper;
 use yii\helpers\Html;
+use yii\helpers\Json;
 use yii\helpers\StringHelper;
 use yii\helpers\Url;
 use yii\web\BadRequestHttpException;
@@ -19,6 +23,11 @@ use yii\web\Response;
 use yii\web\ServerErrorHttpException;
 use yii\web\UploadedFile;
 
+/**
+ * Class ModelController
+ * @property Module $module
+ * @package vladdnepr\ycm\controllers
+ */
 class ModelController extends Controller
 {
     /** @inheritdoc */
@@ -29,7 +38,10 @@ class ModelController extends Controller
                 'class' => AccessControl::className(),
                 'rules' => [
                     [
-                        'actions' => ['index', 'list', 'create', 'update', 'delete', 'redactor-upload', 'redactor-list'],
+                        'actions' => [
+                            'index', 'list', 'create', 'update', 'delete',
+                            'redactor-upload', 'redactor-list', 'choices', 'editable'
+                        ],
                         'allow' => true,
                         'roles' => ['@'],
                         'matchCallback' => function ($rule, $action) {
@@ -452,5 +464,52 @@ class ModelController extends Controller
         }
 
         return $this->redirect(['list', 'name' => $name]);
+    }
+
+    public function actionChoices($name, $q = null, $id = null)
+    {
+        $out = ['results' => ['id' => '', 'text' => '']];
+
+        \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+
+        /* @var ActiveRecord $model */
+        $model = $this->module->ycm->loadModel($name);
+
+        if (!is_null($q)) {
+            $out['results'] = ModelHelper::findChoicesByLabel($model, $q);
+        } elseif ($id > 0) {
+            $out['results'] = [
+                ModelHelper::getPkColumnName($model) => $id,
+                'text' => ModelHelper::getLabelColumnName($model->findOne($id))
+            ];
+        }
+        return $out;
+    }
+
+    public function actionEditable($name)
+    {
+        /** @var $model \yii\db\ActiveRecord */
+        $model = $this->module->loadModel($name);
+
+        if (!\Yii::$app->request->post('hasEditable')) {
+            throw new BadRequestHttpException;
+        }
+
+        $output = '';
+        $message = '';
+
+        $model = $model->findOne(\Yii::$app->request->post('editableKey'));
+
+        $modelShortName = $model->formName();
+        $post = \Yii::$app->request->post($modelShortName);
+        $modelAttributes = current($post);
+
+        if ($model->load([$modelShortName => $modelAttributes])) {
+            if (!$model->save()) {
+                $message = Html::errorSummary($model);
+            }
+        }
+
+        return Json::encode(['output' => $output, 'message' => $message]);
     }
 }
